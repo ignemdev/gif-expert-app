9b1e4acebc784aa7efecd3a9a69ac65e33bfffa8
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-08-22 17:34:32 -0400

    styles added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,11 @@
 export const GifExpertApp = () => {
     return (
         <>
+            {/*Titulo*/ }
             <h1>GifExpertApp</h1>
+            {/*Input*/ }
+            {/*Listado de Gifs*/ }
+            {/*GifItem*/ }
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GifExpertApp } from './GifExpertApp';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
 
+import { GifExpertApp } from './GifExpertApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
